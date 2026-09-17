83688ebb48a9b1b3532f3185eafebaad49b7399f
yousef444555u-maker <yousef444555u@gmail.com>
2026-09-18 02:02:55 +0300

    Update async_bot.py

--- async_bot.py
+++ async_bot.py
@@ -3,7 +3,7 @@ import logging
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -12,9 +12,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"قلت: {update.message.text}")
 
-if __name__ == "__main__":
+def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     print("Bot started...")
     app.run_polling()
+
+if __name__ == "__main__":
+    main()
